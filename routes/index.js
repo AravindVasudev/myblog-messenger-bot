34d0a757b723e840ac9b114f3dd55ef08fa38134
AravindVasudev <aravindv96@yahoo.com>
2017-01-29 10:51:57 +0530

    Stub to check if closure is properly called

--- routes/index.js
+++ routes/index.js
@@ -21,35 +21,7 @@ router.get('/webhook', function(req, res) {
 });
 
 router.post('/webhook', function (req, res) {
-  var data = req.body;
-  console.log(req.body);
-  console.log(req.body.entry.messaging[0].message);
-
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
-    res.sendStatus(200);
-  }
+  console.log('hi');
 });
 
 function receivedMessage(event) {
