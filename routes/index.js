d46c38076021192463e7fd936abaf8a46a284f07
AravindVasudev <aravindv96@yahoo.com>
2017-01-30 05:57:37 +0530

    Added typing indication

--- routes/index.js
+++ routes/index.js
@@ -51,7 +51,8 @@ router.post('/webhook', function (req, res) {
     // You must send back a 200, within 20 seconds, to let us know
     // you've successfully received the callback. Otherwise, the request
     // will time out and we will keep trying to resend.
-    res.sendStatus(200);
+
+    res.json({ recipient:{ id:"USER_ID" }, sender_action:"typing_on" });
   }
 });
 
@@ -107,6 +108,10 @@ function sendGenericMessage(recipientId) {
   };
 
   callSendAPI(messageData);
+}
+
+function sendArticle(recipientId) {
+
 }
 
 function sendTextMessage(recipientId, messageText) {
